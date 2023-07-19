8545ace32989d65843dbe5af5eebb16b6625fc32
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-19 07:34:21 +0700

    Menjalankan Validasi

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -23,6 +23,28 @@ class ValidatorTest extends TestCase
 
         $validator = Validator::make($data, $rules);
         self::assertNotNull($validator);
+
+        self::assertTrue($validator->passes());
+        self::assertFalse($validator->fails());
+    }
+
+    public function testValidatorInvalid()
+    {
+        $data = [
+            "username" => "",
+            "password" => ""
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
     }
 
 }
