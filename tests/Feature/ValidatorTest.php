c8f364bcc17c911432b8a37fc4e7172c1ac0f8ed
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-19 07:31:15 +0700

    Validator

---
+++ tests/Feature/ValidatorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function testValidator()
+    {
+        $data = [
+            "username" => "admin",
+            "password" => "123445"
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+    }
+
+}
